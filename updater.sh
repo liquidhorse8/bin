3c1ddfd286eb86e1d319c79f3027f9d617609437
soo256 <soichiro260@gmail.com>
2020-02-11 06:35:03 +0900

    change

--- updater.sh
+++ updater.sh
@@ -9,8 +9,8 @@ ipアドレスの固定
 chromeログイン
 sambaユーザー追加
 自動起動(conky設定)
-visudo→hoge ALL=NOPASSWD:  mount,umount
-時計設定カスタム %Y/%m/%d %A %H:%M
+visudo →  hoge ALL=NOPASSWD:  mount,umount
+時計設定カスタム %Y/%m/%d %A %H:%M:%S
 外観設定
 ミュート:amixer -D pulse set Master 1+ toggle
 音量アップ:amixer -D pulse set Master 1%+
@@ -18,9 +18,18 @@ visudo→hoge ALL=NOPASSWD:  mount,umount
 out
 
 # aptリポジトリ
-sudo add-apt-repository ppa:nijel/phpmyadmin
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
 # aptパッケージ
-sudo apt install  chromium-browser chromium-browser-l10n emacs vlc ffmpeg  python3-pip python-pip python-gtk2 python- nkf samba unar youtube-dl gthumb xautomation conky conky-all tumbler  php-elisp vlc-l10n vlc-plugin-samba cifs-utils curl gparted tumbler-plugins-extra  eyed3 gimp git
+sudo apt install  chromium-browser chromium-browser-l10n git emacs vlc ffmpeg   nkf samba unar youtube-dl gthumb xautomation conky conky-all tumbler  vlc-l10n vlc-plugin-samba cifs-utils curl gparted tumbler-plugins-extra  eyed3 gimp
+# python
+sudo apt install python3-pip python-pip python-gtk2
+# php
+sudo apt install php7.2 php7.2-common php7.2-cli php7.2-fpm php7.2-mysql php7.2-dev php7.2-mbstring php7.2-zip  php-elisp php5.6-mysql php5.6-mysql
+sudo apt-get install apache2 mysql-server libapache2-mod-php
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
 # VLCでISOの再生
 sudo apt install libdvd-pkg;sudo dpkg-reconfigure libdvd-pkg
 
